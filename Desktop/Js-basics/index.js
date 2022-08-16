9eab468430571f0585037fa7bbe6dddb6c17595a
moegreene5 <agbasiifeanyi@gmail.com>
2022-08-16 18:45:46 +0100

    first commit

---
+++ Desktop/Js-basics/index.js
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+
+=======
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+>>>>>>> ed37dac490dd22b48a16b0b4079f8f6bd7f244a0
